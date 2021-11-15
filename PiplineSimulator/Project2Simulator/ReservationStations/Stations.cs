918106e3e0314b5038058f81b80ba188b2c92c4e
Ajblast <46971018+Ajblast@users.noreply.github.com>
2021-11-15 00:25:35 -0500

    Added new functial station

--- PiplineSimulator/Project2Simulator/ReservationStations/Stations.cs
+++ PiplineSimulator/Project2Simulator/ReservationStations/Stations.cs
@@ -18,17 +18,19 @@ namespace Project2Simulator.ReservationStations
 			// Create all reservation stations
 			int index = 0;
             for (int i = 0; i < counts.MemoryUnit; i++, index++)
-				reservationStations[index] = new ReservationStation(regs, control, FunctionalUnitType.MEMORY_UNIT, core, new StationID(index));
+				reservationStations[index] = new ReservationStation(regs, bus, control, FunctionalUnitType.MEMORY_UNIT, core, new StationID(index));
+            for (int i = 0; i < counts.BranchUnit; i++, index++)
+				reservationStations[index] = new ReservationStation(regs, bus, control, FunctionalUnitType.BRANCH_UNIT, core, new StationID(index));
             for (int i = 0; i < counts.FloatingAdder; i++, index++)
-				reservationStations[index] = new ReservationStation(regs, control, FunctionalUnitType.FLOATING_ADDER, core, new StationID(index));
+				reservationStations[index] = new ReservationStation(regs, bus, control, FunctionalUnitType.FLOATING_ADDER, core, new StationID(index));
             for (int i = 0; i < counts.FloatingMultiplier; i++, index++)
-				reservationStations[index] = new ReservationStation(regs, control, FunctionalUnitType.FLOATING_MULTIPLIER, core, new StationID(index));
+				reservationStations[index] = new ReservationStation(regs, bus, control, FunctionalUnitType.FLOATING_MULTIPLIER, core, new StationID(index));
             for (int i = 0; i < counts.IntegerAdder; i++, index++)
-				reservationStations[index] = new ReservationStation(regs, control, FunctionalUnitType.INTEGER_ADDER, core, new StationID(index));
+				reservationStations[index] = new ReservationStation(regs, bus, control, FunctionalUnitType.INTEGER_ADDER, core, new StationID(index));
             for (int i = 0; i < counts.IntegerMultiplier; i++, index++)
-				reservationStations[index] = new ReservationStation(regs, control, FunctionalUnitType.INTEGER_MULTIPLIER, core, new StationID(index));
+				reservationStations[index] = new ReservationStation(regs, bus, control, FunctionalUnitType.INTEGER_MULTIPLIER, core, new StationID(index));
             for (int i = 0; i < counts.MovementUnit; i++, index++)
-				reservationStations[index] = new ReservationStation(regs, control, FunctionalUnitType.MOVEMENT_UNIT, core, new StationID(index));
+				reservationStations[index] = new ReservationStation(regs, bus, control, FunctionalUnitType.MOVEMENT_UNIT, core, new StationID(index));
 		}
 
 		public void Cycle()
