ede905df7b1283392ecc6ea91e71509f653a4cca
Seth Bowden <79893142+bowdens07@users.noreply.github.com>
2021-11-14 20:04:52 -0500

    ReorderBuffer slot and ReorderBufferID

--- PiplineSimulator/Project2Simulator/ReorderBuffers/ReorderBufferID.cs
+++ PiplineSimulator/Project2Simulator/ReorderBuffers/ReorderBufferID.cs
@@ -1,8 +1,31 @@
+/* Author; Seth Bowden */
 namespace Project2Simulator.ReorderBuffers
 {
 	public class ReorderBufferID
 	{
-	}
+        public int BufferID {get; private set; }
+
+        public ReorderBufferID(int bufferID)
+        {
+            BufferID = bufferID;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() != this.GetType())
+                return false;
+
+            if (((ReorderBufferID) obj).BufferID == this.BufferID)
+                return true;
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
 
 }
 
