eaccbe1b2f45d8aeb649e4591af83c40f1f14eab
Ajblast <46971018+Ajblast@users.noreply.github.com>
2021-11-15 02:06:57 -0500

    Added addresses and core ids

--- PiplineSimulator/Project2Simulator/Address.cs
+++ PiplineSimulator/Project2Simulator/Address.cs
@@ -1,8 +1,30 @@
+/* Author: Austin Kincer */
+
 namespace Project2Simulator
 {
 	public class Address
 	{
-	}
+		public int Value;
+
+        public Address(int Value)
+        {
+            this.Value = Value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || obj.GetType() != typeof(Address))
+                return false;
+
+            return Value == ((Address)obj).Value;
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+    }
 
 }
 
--- PiplineSimulator/Project2Simulator/CoreID.cs
+++ PiplineSimulator/Project2Simulator/CoreID.cs
@@ -1,8 +1,34 @@
+/* Author: Austin Kincer */
+
 namespace Project2Simulator
 {
 	public class CoreID
 	{
-	}
+        public int ID;
+
+        public CoreID(int ID)
+        {
+            this.ID = ID;
+        }
+        public CoreID(CoreID other)
+        {
+            ID = other.ID; ;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || obj.GetType() != typeof(CoreID))
+                return false;
+
+            return ID == ((CoreID)obj).ID;
+        }
+
+        public override int GetHashCode()
+        {
+            return ID.GetHashCode();
+        }
+
+    }
 
 }
 
