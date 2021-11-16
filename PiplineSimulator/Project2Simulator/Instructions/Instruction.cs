dfeae05133d80f2b0a7bc9ace10783d3e9c268ef
Ajblast <46971018+Ajblast@users.noreply.github.com>
2021-11-16 02:32:58 -0500

    Added secondary destination and third operand

--- PiplineSimulator/Project2Simulator/Instructions/Instruction.cs
+++ PiplineSimulator/Project2Simulator/Instructions/Instruction.cs
@@ -10,14 +10,15 @@ namespace Project2Simulator.Instructions
 		public Opcode Opcode;
 
 		public RegisterID Destination;
+		public RegisterID Destination2;
 
 		public RegisterValue Op1;
-
 		public RegisterValue Op2;
+		public RegisterValue Op3;
 
 		public RegisterID Op1Reg;
-
 		public RegisterID Op2Reg;
+		public RegisterID Op3Reg;
 
 		public FunctionalUnitType FunctionalUnitType;
 
@@ -25,15 +26,18 @@ namespace Project2Simulator.Instructions
 
 		public Address Address;
 
-        public Instruction(Opcode opcode, RegisterID dest, RegisterID op1Reg, RegisterID op2Reg, RegisterValue op1Val, RegisterValue op2Val, FunctionalUnitType type)
+        public Instruction(Opcode opcode, RegisterID dest, RegisterID dest2, RegisterID op1Reg, RegisterID op2Reg, RegisterID op3Reg, RegisterValue op1Val, RegisterValue op2Val, RegisterValue op3Val, FunctionalUnitType type)
         {
 			Opcode = opcode;
 			Destination = dest;
+			Destination2 = dest2;
 			Op1Reg = op1Reg;
 			Op2Reg = op2Reg;
+			Op3Reg = op3Reg;
 			FunctionalUnitType = type;
 			Op1 = op1Val;
 			Op2 = op2Val;
+			Op3 = op3Val;
 			RegisterValue = null;
 			Address = null;
         }
