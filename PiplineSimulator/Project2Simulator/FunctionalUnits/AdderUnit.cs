90af85226e807a4f47b32f26b3da2ea07a41ed47
Ajblast <46971018+Ajblast@users.noreply.github.com>
2021-11-16 02:31:53 -0500

    Start Functional Units

--- PiplineSimulator/Project2Simulator/FunctionalUnits/AdderUnit.cs
+++ PiplineSimulator/Project2Simulator/FunctionalUnits/AdderUnit.cs
@@ -1,10 +1,12 @@
-using Project2Simulator.FunctionalUnits;
+using Project2Simulator.Instructions;
+using Project2Simulator.Registers;
+using Project2Simulator.ReorderBuffers;
 
 namespace Project2Simulator.FunctionalUnits
 {
     public abstract class AdderUnit : FunctionalUnit
     {
-        protected AdderUnit(CoreID core) : base(core)
+        protected AdderUnit(THECommonDataBus bus, CoreID core) : base(FunctionalUnitType.INTEGER_ADDER, bus, core)
         {
         }
     }
--- PiplineSimulator/Project2Simulator/FunctionalUnits/FunctionalUnit.cs
+++ PiplineSimulator/Project2Simulator/FunctionalUnits/FunctionalUnit.cs
@@ -1,7 +1,8 @@
 /* Author: Austin Kincer */
 
 using Project2Simulator.ReorderBuffers;
-using System;
+using Project2Simulator.Instructions;
+using Project2Simulator.Registers;
 
 namespace Project2Simulator.FunctionalUnits
 {
@@ -9,9 +10,19 @@ namespace Project2Simulator.FunctionalUnits
 	{
 		public FunctionalUnitType Type;
 
-		private THECommonDataBus bus;
-		private CoreID core;
+		public bool Executing;
 
+		protected THECommonDataBus bus;
+		protected CoreID core;
+
+		protected Opcode opcode;
+		protected RegisterValue op1;
+		protected RegisterValue op2;
+		protected RegisterValue op3;
+
+		protected RegisterValue dest1;
+		protected RegisterValue dest2;
+		protected bool dest2Valid;
 
 		public FunctionalUnit(FunctionalUnitType type, THECommonDataBus bus, CoreID core)
 		{
@@ -20,20 +31,34 @@ namespace Project2Simulator.FunctionalUnits
 			this.core = core;
 		}
 
-		public bool Cycle()
-		{
-			throw new NotImplementedException();
-		}
+		public void StartExecution(Opcode opcode, RegisterValue op1, RegisterValue op2, RegisterValue op3)
+        {
+			this.opcode = opcode;
+			this.op1 = op1;
+			this.op2 = op2;
+			this.op3 = op3;
 
-		public void Flush()
-		{
-			throw new NotImplementedException();
-		}
+			Executing = true;
+        }
 
-		public void Commit(ReorderBufferID id)
-		{
-			throw new NotImplementedException();
-		}
+		public abstract bool Cycle();
+
+		public virtual void Flush()
+        {
+			opcode = Opcode.NOP;
+			op1.Value = 0;
+			op2.Value = 0;
+			op3.Value = 0;
+
+			Executing = false;
+        }
+
+		public virtual void Commit(ReorderBufferID id)
+        {
+			bus.Write(id, dest1, dest2, dest2Valid);
+
+			Executing = false;
+        }
 
 	}
 
