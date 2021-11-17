f4d84db461ec5ff9360a78f3085d9f75ec6ef222
Ajblast <46971018+Ajblast@users.noreply.github.com>
2021-11-17 02:23:34 -0500

    Update reservation station counts

--- PiplineSimulator/Project2Simulator/ReservationStations/ReservationStationCounts.cs
+++ PiplineSimulator/Project2Simulator/ReservationStations/ReservationStationCounts.cs
@@ -4,12 +4,22 @@ namespace Project2Simulator.ReservationStations
 	{
 		public int MemoryUnit;
 		public int BranchUnit;
-		public int FloatingAdder;
 		public int IntegerAdder;
 		public int IntegerMultiplier;
+		public int FloatingAdder;
 		public int FloatingMultiplier;
 		public int MovementUnit;
 
+		public ReservationStationCounts(int memory, int branch, int intAdder, int intMultiplier, int floatAdder, int floatMultiplier, int movement)
+        {
+			MemoryUnit = memory;
+			BranchUnit = branch;
+			IntegerAdder = intAdder;
+			IntegerMultiplier = intMultiplier;
+			FloatingAdder = floatAdder;
+			FloatingMultiplier = floatMultiplier;
+			MovementUnit = movement;
+        }
 		public int Total()
         {
 			return MemoryUnit + BranchUnit + IntegerAdder + IntegerMultiplier + FloatingAdder + FloatingMultiplier + MovementUnit;
