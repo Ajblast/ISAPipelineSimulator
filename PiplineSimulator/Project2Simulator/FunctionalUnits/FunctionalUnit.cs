d142ab31d465a3a7d6c19cc6f72bdc119d0728a7
Ajblast <46971018+Ajblast@users.noreply.github.com>
2021-11-17 02:23:25 -0500

    Create movement unit

--- PiplineSimulator/Project2Simulator/FunctionalUnits/FunctionalUnit.cs
+++ PiplineSimulator/Project2Simulator/FunctionalUnits/FunctionalUnit.cs
@@ -29,6 +29,15 @@ namespace Project2Simulator.FunctionalUnits
 			Type = type;
 			this.bus = bus;
 			this.core = core;
+
+			op1 = new RegisterValue();
+			op2 = new RegisterValue();
+			op3 = new RegisterValue();
+
+			dest1 = new RegisterValue();
+			dest2 = new RegisterValue();
+			dest2Valid = false;
+
 		}
 
 		public void StartExecution(Opcode opcode, RegisterValue op1, RegisterValue op2, RegisterValue op3)
@@ -37,6 +46,10 @@ namespace Project2Simulator.FunctionalUnits
 			this.op1 = op1;
 			this.op2 = op2;
 			this.op3 = op3;
+
+			dest1 = new RegisterValue();
+			dest2 = new RegisterValue();
+			dest2Valid = false;
 
 			Executing = true;
         }
--- PiplineSimulator/Project2Simulator/FunctionalUnits/MovementUnit.cs
+++ PiplineSimulator/Project2Simulator/FunctionalUnits/MovementUnit.cs
@@ -4,8 +4,15 @@ namespace Project2Simulator.FunctionalUnits
 {
     public class MovementUnit : FunctionalUnit
     {
-        public MovementUnit(CoreID core) : base(core)
+        public MovementUnit(THECommonDataBus bus, CoreID core) : base(FunctionalUnitType.MOVEMENT_UNIT, bus, core)
         {
+        }
+
+        public override bool Cycle()
+        {
+            dest1.Value = op1.Value;
+
+            return true;
         }
     }
 
