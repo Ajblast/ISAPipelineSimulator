25d5ee6f0582e075031d9135ca98729e23d0a6bd
Seth Bowden <79893142+bowdens07@users.noreply.github.com>
2021-11-22 22:02:08 -0500

    Implement functional factory

--- PiplineSimulator/Project2Simulator/FunctionalUnits/FunctionalFactory.cs
+++ PiplineSimulator/Project2Simulator/FunctionalUnits/FunctionalFactory.cs
@@ -18,7 +18,28 @@ namespace Project2Simulator.FunctionalUnits
 
 		public static FunctionalUnit CreateUnit(THECommonDataBus bus, FunctionalUnitType type, CoreID id)
         {
-            throw new NotImplementedException();
+            switch (type)
+            {
+                case FunctionalUnitType.NULL:
+                    return null;
+                case FunctionalUnitType.MEMORY_UNIT:
+                    return new MemoryUnit(mmu, cache, bus, id);
+                case FunctionalUnitType.BRANCH_UNIT:
+                    return new BranchUnit(bus, id);
+                case FunctionalUnitType.FLOATING_ADDER:
+                    return new FloatingAdder(bus, id);
+                case FunctionalUnitType.FLOATING_MULTIPLIER:
+                    return new FloatingMultiplier(bus, id);
+                case FunctionalUnitType.INTEGER_ADDER:
+                    return new IntegerAdder(bus, id);
+                case FunctionalUnitType.INTEGER_MULTIPLIER:
+                    return new IntegerMultiplier(bus, id);
+                case FunctionalUnitType.MOVEMENT_UNIT:
+                    return new MovementUnit(bus, id);
+                default:
+                case FunctionalUnitType.ILLEGAL:
+                    throw new Exception("Asking to build an illegal functional unit type");
+            }
         }
 	}
 
