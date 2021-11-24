9b5b98dcbd2304e99b1006ebb53ec9c38ad155e1
Ajblast <46971018+Ajblast@users.noreply.github.com>
2021-11-24 04:32:53 -0500

    Added register helper

---
+++ PiplineSimulator/Project2Simulator/Registers/RegisterHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Project2Simulator.Registers
+{
+    public enum RegisterName : byte
+    {
+        RA = 0,
+        RB,
+        RC,
+        RD,
+        RE,
+        RF,
+        RG,
+        RH,
+        RI,
+        RJ,
+        RK,
+        RL,
+        RM,
+        PC,
+        SP,
+        FLAG
+    }
+    public static class RegisterHelper
+    {
+        public static RegisterName StringToName(string name)
+        {
+            return (RegisterName)Enum.Parse(typeof(RegisterName), name, true);
+        }
+
+    }
+}
