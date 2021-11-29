8ddbbb21ea11a69cd2e75e96bac9217411143df8
Seth Bowden <79893142+bowdens07@users.noreply.github.com>
2021-11-29 03:09:21 -0500

    reformat

--- PiplineSimulator/StringFormatService.cs
+++ PiplineSimulator/StringFormatService.cs
@@ -8,7 +8,7 @@ namespace PiplineSimulator
 {
     public static class StringFormatService
     {
-        public static string GetInstructionFormat() { return "{0,-5:X}{1,-4:X2}{2,-4:X2}{3,-6:X2}{4,-9:X8}{5,-6:X2}{6,-9:X8}{7,-6:x2}{8,-9:X8}{9,-6:x5}{10}"; }
+        public static string GetInstructionFormat() { return "{0,-6:X}{1,-4:X2}{2,-4:X2}{3,-6:X2}{4,-9:X8}{5,-6:X2}{6,-9:X8}{7,-6:x2}{8,-9:X8}{9,-6:x5}{10}"; }
 
         public static string GetReorderBufferSlotFormat() { return "{0,-4:X}{1,-4:X}{2,-5:X}{3,-6}{4,-3:X2}{5,-9:X8}{6,-7}{7,-3:X2}{8,-9:X8}{9}"; }
 
